c0cb1153e9aee91a6475cca3cc19796b2d67b960
Aryan Parmar <notaarryan@users.noreply.github.com>
2025-05-27 21:26:24 -0400

    Implemented resetGrid function
    completed the implementation of resetGrid function which takes a newGridSize and first removes the old grid and then makes a new one according to newGridSize

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let createGrid = (color = "black", gridSize = 16) => {
+let createGrid = (gridSize = 16, color = "black") => {
   for (let row = 0; row < gridSize; row++) {
     for (let col = 0; col < gridSize; col++) {
       const gridElement = document.createElement("div");
@@ -19,5 +19,29 @@ let createGrid = (color = "black", gridSize = 16) => {
   }
 };
 
+let resetGridSize = (newGridSize) => {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild);
+  }
+  createGrid((gridSize = newGridSize));
+};
+
+let gridSize = 16;
+let newGridSize = 0;
 const container = document.querySelector(".container");
 createGrid();
+const gridSizingButton = document.querySelector(".gridSizingButton");
+gridSizingButton.addEventListener("click", () => {
+  let newGridSize = parseInt(prompt("Enter grid size (Max: 100)"));
+  console.log(newGridSize);
+
+  if (isNaN(newGridSize)) {
+    alert("No input received");
+  } else if (newGridSize <= 0 || newGridSize > 100) {
+    alert("Invalid grid size");
+  } else {
+    resetGridSize(newGridSize);
+    gridSize = newGridSize;
+    newGridSize = 0;
+  }
+});
